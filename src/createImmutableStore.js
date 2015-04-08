4f49a0f7300657fc1aada24493040fb73afa5da9
Ryan Sukale <rysukale@yahoo-inc.com>
2015-04-07 18:46:07 -0700

    Only added the mergeState while retaining previous code

--- src/createImmutableStore.js
+++ src/createImmutableStore.js
@@ -9,11 +9,41 @@ var createStore = require('fluxible/utils/createStore');
 var Immutable = require('immutable');
 
 function merge(dest, src) {
-    Object.keys(src).forEach(function (prop) {
+    Object.keys(src).forEach(function cb(prop) {
         dest[prop] = src[prop];
     });
 
     return dest;
+}
+
+function initialize() {
+    this._state = Immutable.Map();
+}
+
+function rehydrate(state) {
+    this._state = Immutable.fromJS(state);
+}
+
+function dehydrate() {
+    return this._state;
+}
+
+function setState(newState, event, payload) {
+    if (this._state === newState) {
+        return false;
+    }
+
+    this._state = Immutable.fromJS(newState);
+    event ? this.emit(event, payload) : this.emitChange(payload);
+    return true;
+}
+
+function mergeState(stateFragment, event, payload) {
+    return this.setState(
+        this._state.merge(stateFragment),
+        event,
+        payload
+    );
 }
 
 /**
@@ -25,41 +55,13 @@ function merge(dest, src) {
  * @param {Object} spec.handlers Hash of action name to method name of action handlers
  * @param {Function} [spec.initialize] Function called during construction for setting the default `_state` (optional).
  * @return {Store} Store class
- */
+ **/
 module.exports = function createImmutableStore(spec) {
     return createStore(merge({
-        initialize: function initialize() {
-            this._state = Immutable.Map();
-        },
-
-        rehydrate: function rehydrate(state) {
-            this._state = Immutable.fromJS(state);
-        },
-
-        dehydrate: function dehydrate() {
-            return this._state;
-        },
-
-        setState: function setState(newState, event, payload) {
-            if (this._state === newState) {
-                return false;
-            }
-
-            this._state = newState;
-            if (event) {
-                this.emit(event, payload);
-            } else {
-                this.emitChange(payload);
-            }
-            return true;
-        },
-
-        mergeState: function mergeState(stateFragment, event, payload) {
-            return this.setState(
-                this._state.merge(stateFragment),
-                event,
-                payload
-            );
-        }
+        initialize: initialize,
+        rehydrate: rehydrate,
+        dehydrate: dehydrate,
+        setState: setState,
+        mergeState: mergeState
     }, spec));
 };
