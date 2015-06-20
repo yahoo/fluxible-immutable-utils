4648cd395e9d3a2422cf652942959a3f9e279762
Ed Bosher <edbosher@gmail.com>
2015-06-20 16:27:55 +0800

    Give access to the createImmutableContainer in an es6 include

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     ComponentMixin: require('./mixins/ImmutableMixin'),
     createImmutableMixin: require('./lib/createImmutableMixin'),
     createImmutableStore: require('./lib/createImmutableStore'),
+    createImmutableContainer: require('./lib/createImmutableContainer'),
     ImmutableStore: require('./lib/ImmutableStore')
 };
