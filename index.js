7142b88d8e7802959f1afb2991681912e55bb629
Ed Bosher <ed@bitgamelabs.com>
2015-11-13 12:06:08 +0800

    Bump engine and still reference createImmutableStore

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     ComponentMixin: require('./mixins/ImmutableMixin'),
     createImmutableMixin: require('./lib/createImmutableMixin'),
     createImmutableContainer: require('./lib/createImmutableContainer'),
+    createImmutableStore: require('./lib/createImmutableStore'),
     ImmutableStore: require('./lib/ImmutableStore')
 };
